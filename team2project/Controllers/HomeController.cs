5ec3f8f1ca617de7508d5da7effc0f9cd9971df0
milbro <milbro1995@gmail.com>
2015-05-02 19:38:09 +0300

    added:     HomeController.cs     Details.cshtml     Index.cshtml     List.cshtml

---
+++ team2project/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using team2project.Models;
+
+namespace team2project.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Home/List
+        [HttpGet]
+        public ActionResult List(HomeModel homeModel)
+        {
+            var model = homeModel;
+            ViewBag.list = model.GetEvents();
+            return View();
+        }
+
+        // GET: /Home/List
+        [HttpGet]
+        public ActionResult Details(uint id, HomeModel homeModel)
+        {
+            var model = homeModel;
+            ViewBag.curEvent = model.GetById(id);
+            return View();
+        }
+    }
+}
