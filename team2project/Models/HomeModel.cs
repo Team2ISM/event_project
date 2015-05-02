8f9b8c988c0b0726b36e4b3ef3293a4608a91dfe
NewGeib <iviviv1901@gmail.com>
2015-05-03 00:01:49 +0300

    +Long Description, public fields ~> properties

--- team2project/Models/HomeModel.cs
+++ team2project/Models/HomeModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,13 +7,14 @@ namespace team2project.Models
 {
     public class Event
     {
-        public uint id;
-        public string title;
-        public string desc;
-        public DateTime from;
-        public DateTime to;
-        public string location;
-        public Event(uint id, string title, string desc, DateTime from, DateTime to, string location)
+        public uint id { get; set; }
+        public string title { get; set; }
+        public string desc { get; set; }
+        public DateTime from { get; set; }
+        public DateTime to { get; set; }
+        public string location { get; set; }
+        public string longDesc { get; set; }
+        public Event(uint id, string title, string desc, DateTime from, DateTime to, string location, string longDesc = "")
         {
             this.id = id;
             this.title = title;
@@ -21,6 +22,7 @@ namespace team2project.Models
             this.from = from;
             this.to = to;
             this.location = location;
+            this.longDesc = longDesc;
         }
     }
     public class HomeModel
@@ -30,15 +32,15 @@ namespace team2project.Models
         {
             this.eventList = new Dictionary<uint, Event>();
             eventList.Add(1, new Event(1, "title1", "desc1", new DateTime(2015, 06, 1, 12, 0, 0), new DateTime(2015, 06, 1, 13, 0, 0), "zt"));
-            eventList.Add(2, new Event(1, "title2", "desc2", new DateTime(2015, 06, 1, 13, 0, 0), new DateTime(2015, 06, 1, 14, 0, 0), "zt"));
-            eventList.Add(3, new Event(1, "title3", "desc3", new DateTime(2015, 06, 1, 14, 0, 0), new DateTime(2015, 06, 1, 15, 0, 0), "zt"));
-            eventList.Add(4, new Event(1, "title4", "desc4", new DateTime(2015, 06, 1, 15, 0, 0), new DateTime(2015, 06, 1, 16, 0, 0), "zt"));
-            eventList.Add(5, new Event(1, "title5", "desc5", new DateTime(2015, 06, 1, 16, 0, 0), new DateTime(2015, 06, 1, 17, 0, 0), "zt"));
-            eventList.Add(6, new Event(1, "title6", "desc6", new DateTime(2015, 06, 2, 12, 0, 0), new DateTime(2015, 06, 1, 13, 0, 0), "zt"));
-            eventList.Add(7, new Event(1, "title7", "desc7", new DateTime(2015, 06, 2, 13, 0, 0), new DateTime(2015, 06, 1, 15, 0, 0), "zt"));
-            eventList.Add(8, new Event(1, "title8", "desc8", new DateTime(2015, 06, 2, 15, 0, 0), new DateTime(2015, 06, 1, 16, 0, 0), "zt"));
-            eventList.Add(9, new Event(1, "title9", "desc9", new DateTime(2015, 06, 3, 16, 0, 0), new DateTime(2015, 06, 1, 17, 30, 0), "zt"));
-            eventList.Add(10, new Event(1, "title10", "desc10", new DateTime(2015, 06, 3, 18, 0, 0), new DateTime(2015, 06, 1, 19, 0, 0), "zt"));
+            eventList.Add(2, new Event(2, "title2", "desc2", new DateTime(2015, 06, 1, 13, 0, 0), new DateTime(2015, 06, 1, 14, 0, 0), "zt"));
+            eventList.Add(3, new Event(3, "title3", "desc3", new DateTime(2015, 06, 1, 14, 0, 0), new DateTime(2015, 06, 1, 15, 0, 0), "zt"));
+            eventList.Add(4, new Event(4, "title4", "desc4", new DateTime(2015, 06, 1, 15, 0, 0), new DateTime(2015, 06, 1, 16, 0, 0), "zt"));
+            eventList.Add(5, new Event(5, "title5", "desc5", new DateTime(2015, 06, 1, 16, 0, 0), new DateTime(2015, 06, 1, 17, 0, 0), "zt"));
+            eventList.Add(6, new Event(6, "title6", "desc6", new DateTime(2015, 06, 2, 12, 0, 0), new DateTime(2015, 06, 1, 13, 0, 0), "zt"));
+            eventList.Add(7, new Event(7, "title7", "desc7", new DateTime(2015, 06, 2, 13, 0, 0), new DateTime(2015, 06, 1, 15, 0, 0), "zt"));
+            eventList.Add(8, new Event(8, "title8", "desc8", new DateTime(2015, 06, 2, 15, 0, 0), new DateTime(2015, 06, 1, 16, 0, 0), "zt"));
+            eventList.Add(9, new Event(9, "title9", "desc9", new DateTime(2015, 06, 3, 16, 0, 0), new DateTime(2015, 06, 1, 17, 30, 0), "zt"));
+            eventList.Add(10, new Event(10, "title10", "desc10", new DateTime(2015, 06, 3, 18, 0, 0), new DateTime(2015, 06, 1, 19, 0, 0), "zt"));
         }
 
         public Dictionary<uint, Event> GetEvents()
