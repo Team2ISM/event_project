a44a9e076fc23cb9f8d34a6c3dfa3df3342e85d4
NewGeib <iviviv1901@gmail.com>
2015-05-05 16:48:23 +0300

    Short links(without Home)

--- team2project/App_Start/RouteConfig.cs
+++ team2project/App_Start/RouteConfig.cs
@@ -12,7 +12,11 @@ namespace team2project
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                name: "Index",
+                url: "{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
