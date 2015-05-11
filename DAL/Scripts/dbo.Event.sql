1aef8b298c21d12446ed29ccb8a484d15388239d
BorysMiller <milbro1995@gmail.com>
2015-05-11 23:17:00 +0300

    sql script (creating Event table)

---
+++ DAL/Scripts/dbo.Event.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Event] (
+    [Id]              NVARCHAR (50) NOT NULL,
+    [Title]           NVARCHAR (50) NULL,
+    [Description]     NVARCHAR (50) NULL,
+    [LongDescription] TEXT          NULL,
+    [FromDate]        DATETIME      NULL,
+    [ToDate]          DATETIME      NULL,
+    [Location]        NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
