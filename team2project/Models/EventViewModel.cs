501610791c4c6664d247e6ee25bfb4be943586d0
BilyachenkoOY <iviviv1901@gmail.com>
2015-05-12 20:59:22 +0300

    form attributes

--- team2project/Models/EventViewModel.cs
+++ team2project/Models/EventViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 namespace team2project.Models
 {
     public class EventViewModel
@@ -10,18 +11,29 @@ namespace team2project.Models
             From = DateTime.Now.AddDays(1);
             To = DateTime.Now.AddDays(1);
         }
+
+        [ScaffoldColumn(false)]
         public string Id { get; set; }
 
+        [Required (ErrorMessage="This field is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Lenght must be between 3 and 50")]
         public string Title { get; set; }
 
+        [Required (ErrorMessage = "This field is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Lenght must be between 3 and 50")]
         public string Description { get; set; }
 
+        [DataType(DataType.DateTime)]
         public DateTime? From { get; set; }
 
+        [DataType(DataType.DateTime)]
         public DateTime? To { get; set; }
 
+        [Required (ErrorMessage = "This field is required")]
         public string Location { get; set; }
 
+        [Required (ErrorMessage = "This field is required")]
+        [DataType(DataType.MultilineText)]
         public string LongDescription { get; set; }
 
     }
