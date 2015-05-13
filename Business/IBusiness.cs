bc4e67802387b3a2ccd4ba7582d63ddec20c366d
BorysMiller <milbro1995@gmail.com>
2015-05-13 19:21:07 +0300

    add files

---
+++ Business/IBusiness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public interface IBusiness<TViewModel, TModel>
+    {
+        IList<TViewModel> GetList();
+
+        void Create(TViewModel viewModel);
+
+        TViewModel GetById(string id);
+    }
+}
