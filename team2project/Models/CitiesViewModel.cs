c132eb84a78063dcdd497737fa0cae6b7ca5cebd
BilyachenkoOY <iviviv1901@gmail.com>
2015-05-20 16:05:50 +0300

    Validation messages

--- team2project/Models/CitiesViewModel.cs
+++ team2project/Models/CitiesViewModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Web.UI;
 
 namespace team2project.Models
 {
     public class CitiesViewModel
     {
+        [ScaffoldColumn(false)]
         public virtual string Id { get; set; }
 
+        [Required(ErrorMessage = "Это поле должно быть заполненым")]
+        [StringLength(50, ErrorMessage = "Длина должна быть не больше 50 символов")]
         public virtual string City { get; set; }
     }
 }
