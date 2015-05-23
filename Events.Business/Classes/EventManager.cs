9d13bab88763462ddec1d10df7622376bc9fc9fe
mrrbodia <02bodia20@ukr.net>
2015-05-23 23:52:51 +0300

    Admin Page
    correct code

--- Events.Business/Classes/EventManager.cs
+++ Events.Business/Classes/EventManager.cs
@@ -23,7 +23,7 @@ namespace Events.Business.Classes
             return cacheManager.FromCache<IList<Event>>("allEvents",
                     () =>
                     {
-                        return dataProvider.GetAllEvents();
+                        return dataProvider.GetList(null,null,"Admin");
                     });
         }
         public IList<Event> GetList()
@@ -70,7 +70,7 @@ namespace Events.Business.Classes
         public void ToggleButtonStatusActive(string id)
         {
             dataProvider.ToggleButtonStatusActive(id);
-            cacheManager.RemoveFromCache(id);
+            cacheManager.RemoveFromCache("eventList");
             cacheManager.RemoveFromCache("allEvents");
             cacheManager.ClearCacheByRegion("Filter");
         }
@@ -78,13 +78,14 @@ namespace Events.Business.Classes
         public void ToggleButtonStatusChecked(string id)
         {
             dataProvider.ToggleButtonStatusChecked(id);
-            cacheManager.RemoveFromCache(id);
+            cacheManager.RemoveFromCache("eventList");
+            cacheManager.RemoveFromCache("allEvents");
             cacheManager.ClearCacheByRegion("Filter");
         }
         public void Delete(string id)
         {
             dataProvider.Delete(dataProvider.GetById(id));
-            cacheManager.RemoveFromCache(id);
+            cacheManager.RemoveFromCache("eventList");
             cacheManager.ClearCacheByRegion("Filter");
         }
     }
