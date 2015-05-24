2c84d6b80c04955af47d66609c7121184aad677d
Vlad Stepchin <vladmasterman@gmail.com>
2015-05-24 08:17:08 +0300

    Extended max length

--- team2project/Models/EventViewModel.cs
+++ team2project/Models/EventViewModel.cs
@@ -21,7 +21,7 @@ namespace team2project.Models
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Это поле должно быть заполненым")]
-        [StringLength(50, MinimumLength = 5, ErrorMessage = "Длина должна быть от 5 до 50 символов")]
+        [StringLength(MinimumLength = 5, ErrorMessage = "Длина должна быть от 5 до 50 символов")]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
