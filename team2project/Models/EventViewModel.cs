ea839e6d2b4b4167ac9f360ef1c0d77de4be6ffa
BilyachenkoOY <iviviv1901@gmail.com>
2015-05-24 17:24:36 +0300

    return subscription to main proj + comment

--- team2project/Models/EventViewModel.cs
+++ team2project/Models/EventViewModel.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 using System.Web.UI;
 using Microsoft.Ajax.Utilities;
-using Foolproof;
+/*using Foolproof;*/
 namespace team2project.Models
 {
     public class EventViewModel
@@ -33,7 +33,7 @@ namespace team2project.Models
 
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "Это поле должно быть заполненым")]
-        [GreaterThanOrEqualTo("FromDate",ErrorMessage="Даты введены неправильно")]
+        //[GreaterThanOrEqualTo("FromDate",ErrorMessage="Даты введены неправильно")]
         public DateTime? ToDate { get; set; }
 
         [Required(ErrorMessage = "Это поле должно быть заполненым")]
