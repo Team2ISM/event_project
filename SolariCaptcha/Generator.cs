6cdd2f56c478b811a267c3178f173268b4b1bca7
AlexSolari <alexsanify@gmail.com>
2015-05-26 15:32:25 +0300

    [untested] Added Captcha

---
+++ SolariCaptcha/Generator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolariCaptcha
+{
+    static class Generator
+    {
+        public static string GetCaptcha(int number, string url)
+        {
+            var result = "";
+            var temp = number.ToString();
+            foreach(var element in temp)
+            {
+                result += "<img src=\"http://"+url+"/Content/Captcha/" + element + ".png\">";
+            }
+            return result;
+        }
+
+        public static int GetNumber(int length)
+        {
+            var generator = new Random();
+            var result = 0;
+            for (; length > 0; length--)
+            {
+                result *= 10;
+                result += generator.Next(10);
+            }
+            return result;
+        }
+    }
+}
---
+++ SolariCaptcha/HtmlHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SolariCaptcha
+{
+    public static class CaptchaExtension
+    {
+        public static IHtmlString Captcha(this HtmlHelper helper, int length, string url)
+        {
+            var idInput = Guid.NewGuid().ToString();
+            var idCheckBox = Guid.NewGuid().ToString();
+            var idValidationMsg = Guid.NewGuid().ToString();
+            var functionName = new string(Guid.NewGuid().ToString().ToCharArray().Where(n => char.IsLetter(n)).ToArray()); 
+            var key = new Random().Next(int.MaxValue); 
+            int number = Generator.GetNumber(length);
+            string numbersHtmlCode = Generator.GetCaptcha(number, url);
+            string newLine = "<br>";
+            string textbox = "<input type=\"text\" id=\"" + idInput + "\" oninput=\" document.getElementById('" + idValidationMsg + "').innerHTML = ''; " + functionName + "(this.value)\" required>";
+            string checkboxhidden = "<input type=\"checkbox\" id=\"" + idCheckBox + "\" style=\"display: none\" required>";
+            string validationspan = "<span class=\"field-validation-error\" id=\""+ idValidationMsg +"\"></span>";
+            string script = "<script type=\"text/javascript\">var " + functionName + " = function (a) { a = a ^ " + key + "; var r = " + (number ^ Convert.ToInt32(key)).ToString() + "; document.getElementById('" + idCheckBox + "').checked = (a == r); if (a != r) document.getElementById('" + idValidationMsg + "').innerHTML = 'Неверно введена капча!';} </script>";
+            return new HtmlString(script + numbersHtmlCode + newLine + textbox + checkboxhidden + newLine + validationspan);
+        }
+
+        public static IHtmlString Captcha(this HtmlHelper helper, string url)
+        {
+            var length = new Random().Next(4, 7);
+            var idInput = Guid.NewGuid().ToString();
+            var idCheckBox = Guid.NewGuid().ToString();
+            var idValidationMsg = Guid.NewGuid().ToString();
+            var functionName = new string(Guid.NewGuid().ToString().ToCharArray().Where(n => char.IsLetter(n)).ToArray()); 
+            var key = new Random().Next(int.MaxValue); 
+            int number = Generator.GetNumber(length);
+            string numbersHtmlCode = Generator.GetCaptcha(number, url);
+            string newLine = "<br>";
+            string textbox = "<input type=\"text\" id=\"" + idInput + "\" oninput=\" document.getElementById('" + idValidationMsg + "').innerHTML = ''; " + functionName + "(this.value)\" placeholder=\"Введите цифру с картинки\" required>";
+            string checkboxhidden = "<input type=\"checkbox\" id=\"" + idCheckBox + "\" style=\"display: none\" required>";
+            string validationspan = "<span class=\"field-validation-error\" id=\""+ idValidationMsg +"\"></span>";
+            string script = "<script type=\"text/javascript\">var " + functionName + " = function (a) { a = a ^ " + key + "; var r = " + (number ^ Convert.ToInt32(key)).ToString() + "; document.getElementById('" + idCheckBox + "').checked = (a == r); if (a != r) document.getElementById('" + idValidationMsg + "').innerHTML = 'Неверно введена капча!';} </script>";
+            return new HtmlString(script + numbersHtmlCode + newLine + textbox + checkboxhidden + newLine + validationspan);
+        }
+    }
+}
