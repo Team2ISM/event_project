6f6630e9c65fcad45212e177ca440abc80123493
AlexSolariPC <gearbots@yandex.com>
2015-05-27 17:07:45 +0300

    Fixes
    Repaired comments validation, added header for comment field, reworked
    captcha a little. Also uninstalled unused captcha lib

--- SolariCaptcha/HtmlHelper.cs
+++ SolariCaptcha/HtmlHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,11 +14,10 @@ namespace SolariCaptcha
             var key = new Random().Next(int.MaxValue); 
             int number = Generator.GetNumber(length);
             string numbersHtmlCode = Generator.GetCaptcha(number, url);
-            string newLine = "<br>";
-            string textbox = "<input type=\"text\" id=\"" + idInput + "\" oninput=\" document.getElementById('" + idValidationMsg + "').innerHTML = '';\" placeholder=\"Введите цифру с картинки\" required>";
+            string textbox = "<input type=\"text\" class=\"text-box single-line\" id=\"" + idInput + "\" oninput=\" document.getElementById('" + idValidationMsg + "').innerHTML = '';\" placeholder=\"Введите цифру с картинки\">";
             string validationspan = "<span class=\"field-validation-error\" id=\""+ idValidationMsg +"\"></span>";
             string script = "<script type=\"text/javascript\">var confirmCaptcha = function (a) { a = a ^ " + key + "; var r = " + (number ^ Convert.ToInt32(key)).ToString() + "; if (a != r) document.getElementById('" + idValidationMsg + "').innerHTML = 'Неверно введена капча!'; return (a == r);} </script>";
-            return new HtmlString(script + numbersHtmlCode + newLine + textbox + newLine + validationspan);
+            return new HtmlString(script + numbersHtmlCode + textbox + validationspan);
         }
     }
 }
