9097832745dc45c0101de4c8f3e6bee722304a63
BilyachenkoOY <iviviv1901@gmail.com>
2015-05-27 12:17:07 +0300

    restoring date

--- team2project/Scripts/createScript.js
+++ team2project/Scripts/createScript.js
@@ -136,18 +136,18 @@ window.onload = function(){
     var combobox = $("#combobox");
     combobox.combobox();
     locat.val(combobox.children()[0].innerHTML);
-    //locat.html(combobox.children()[0].innerHTML);
-    jQuery('#datetimepicker1').datetimepicker({
-        timepicker: true,
+    var pick1 = $('#datetimepicker1'), pick2 = $('#datetimepicker2');
+    if (fDate) pick1.val(fDate);
+    if (tDate) pick2.val(tDate);
+    pick1.datetimepicker({
         minDate: '-1970/01/01',
-        startTime: fromDate.toString,
+        startDate: fDate ? fDate.split(' ')[0] : '',
         lang:'ru',
         mask: true
     });
-    jQuery('#datetimepicker2').datetimepicker({
-        timepicker: true,
-        startTime: toDate.toString,
+    pick2.datetimepicker({
         minDate: '-1970/01/01',//
+        startDate: tDate ? tDate.split(' ')[0] : '',
         lang: 'ru',
         mask: true
     });
