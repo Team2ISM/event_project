288768a2d711fa82f88ece106b93f613b32c6f44
AlexSolari <alexsanify@gmail.com>
2015-05-29 19:21:46 +0300

    A lot of fixes
    blablabla i dont remember all of them

---
+++ team2project/Helpers/StringHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace team2project.Helpers
+{
+    public static class StringHelper
+    {
+        public static string Cut(this string data)
+        {
+            var result = "";
+            var words = data.Split(' ');
+            if (words.Length <= 10 && data.Length <= 50)
+            {
+                return data;
+            }
+            else
+            {
+                if (data.Length > 50)
+                {
+                    result = data.Substring(0, 50);
+                }
+                else 
+                    for (var i = 0; i != 10; i++)
+                    {
+                        result += " " + words[i];
+                    }
+                result += "...";
+                return result;
+            }
+
+        }
+    }
+}
