fe56a0815933dd14875d4024ddb116ddadc36584
LeroyDjenkins <another0ne@mail.ru>
2015-06-02 04:32:45 +0300

    SubscribersStyleFix

--- team2project/Global.asax.cs
+++ team2project/Global.asax.cs
@@ -13,8 +13,6 @@ using NHibernate.Context;
 
 namespace team2project
 {
-    // Примечание: Инструкции по включению классического режима IIS6 или IIS7 
-    // см. по ссылке http://go.microsoft.com/?LinkId=9394801
 
     public class MvcApplication : System.Web.HttpApplication
     {
