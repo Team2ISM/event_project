590a7cdb638916cde07656218a99b8a1377a1eb5
AlexSolari @ PC <gearbots@yandex.com>
2015-06-04 20:53:57 +0300

    Error msg fix
    50 -> 6000

--- team2project/Models/EventViewModel.cs
+++ team2project/Models/EventViewModel.cs
@@ -27,7 +27,7 @@ namespace team2project.Models
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Это поле должно быть заполненым")]
-        [StringLength(6000,MinimumLength = 5, ErrorMessage = "Длина должна быть от 5 до 50 символов")]
+        [StringLength(6000, MinimumLength = 5, ErrorMessage = "Длина должна быть от 5 до 6000 символов")]
         [AllowHtml]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
