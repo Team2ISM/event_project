8e6797ffb75a17ce0d4c9ea155fb0644fc4da87a
AlexSolari <alexsanify@gmail.com>
2015-06-05 15:33:41 +0300

    "subscribe" spam fixed, added return url to event

--- team2project/Scripts/details.js
+++ team2project/Scripts/details.js
@@ -5,22 +5,28 @@
 }
 function subscribe() {
     button.html('Подождите');
-    button.on('click', function(){});
+    button.on('click', function () { });
+    button.addClass("disabled");
+    button.attr("disabled", true);
     $.post("/subscribe", { id: id }, function (data) {
         if (data) {
             button.html('Покинуть');
             button.get()[0].onclick = unsubscribe;
         }
         else {
-            location.assign('/User/Login');
+            location.assign('/User/Login?returnURL='+returnURL);
         }
         reloadCount();
         ReloadSubscr($('#subscribers_onhover'));
+        button.removeClass("disabled");
+        button.removeAttr("disabled");
     });
 }
 function unsubscribe() {
     button.html('Подождите');
     button.on('click', function () { });
+    button.addClass("disabled");
+    button.attr("disabled", true);
     $.post("/unsubscribe", { id: id }, function (data) {
         if (data) {
             button.html('Присоединиться');
@@ -31,11 +37,15 @@ function unsubscribe() {
         }
         reloadCount();
         ReloadSubscr($('#subscribers_onhover'));
+        button.removeClass("disabled");
+        button.removeAttr("disabled");
     });
 }
 var button, id, link;
 window.onload = function () {
     button = $('#submit');
+    button.addClass("disabled");
+    button.attr("disabled", true);
     link = $('#subscribers a');
     id = location.pathname.split('/').pop();
     ReloadSubscr($('#subscribers_onhover'));
@@ -49,6 +59,8 @@ window.onload = function () {
             button.html('Присоединиться');
             button.get()[0].onclick = subscribe;
         }
+        button.removeClass("disabled");
+        button.removeAttr("disabled");
     });
 }
 
