688f4a3ac06de74cd462226a0a3bff64555e5038
BorysMiller <milbro1995@gmail.com>
2015-06-08 15:34:28 +0300

    move providers to directory "data providers"

---
+++ Events.NHibernateDataProvider/DataProviders/NHibernateEventDataProvider.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+using NHibernate;
+using Events.Business.Interfaces;
+using Events.Business.Models;
+using Events.Business.Classes;
+using System;
+using NHibernate.Criterion;
+
+
+namespace Events.NHibernateDataProvider.NHibernateCore
+{
+    public class NHibernateEventDataProvider : IEventDataProvider
+    {
+        public IList<Event> GetList(int daysToEvent, string location, string onlyAvailableData, bool isForAdmin = false)
+        {
+            using (ISession session = Helper.OpenSession())
+            {
+                var now = DateTime.Now.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+                if (onlyAvailableData != null)
+                {
+                    var result = session.CreateCriteria<Event>();
+                    if (isForAdmin) result.AddOrder(Order.Asc("Checked"));
+                    return result
+                        .AddOrder(Order.Desc("DateOfCreation"))
+                        .List<Event>();
+                }
+
+                if (!String.IsNullOrEmpty(location))
+                {
+                    session.EnableFilter("equalLocation").SetParameter("chosenLocation", location);
+                }
+
+                if (daysToEvent > 0)
+                {
+                    session.EnableFilter("equalDate").SetParameter("nowaday", now).SetParameter("chosenDate", now.AddDays(daysToEvent - 1));
+                }
+                else
+                {
+                    session.EnableFilter("effectiveDate").SetParameter("asOfDate", now);
+                }
+
+                var criteria = session.CreateCriteria<Event>();
+                criteria.Add(Restrictions.Eq("Active", true));
+                criteria.AddOrder(Order.Asc("FromDate"));
+                return criteria.List<Event>();
+            }
+        }
+
+        public IList<Event> GetByAuthorMail(string email)
+        {
+            using (ISession session = Helper.OpenSession())
+            {
+                var criteria = session.CreateCriteria(typeof(Event));
+                criteria.Add(Restrictions.Eq("AuthorId", email));
+                return criteria.List<Event>();
+            }
+        }
+
+        public IList<Event> GetAuthorPastEvents(string email)
+        {
+            using (ISession session = Helper.OpenSession())
+            {
+                var criteria = session.CreateCriteria(typeof(Event));
+                criteria.Add(Restrictions.And(Restrictions.Eq("AuthorId", email),
+                    Restrictions.Lt("ToDate", DateTime.Now)));
+                return criteria.List<Event>();
+            }
+        }
+
+        public IList<Event> GetAuthorFutureEvents(string email)
+        {
+            using (ISession session = Helper.OpenSession())
+            {
+                var criteria = session.CreateCriteria(typeof(Event));
+                criteria.Add(Restrictions.And(Restrictions.Eq("AuthorId", email),
+                    Restrictions.Ge("ToDate", DateTime.Now)));
+                return criteria.List<Event>();
+            }
+        }
+
+        public Event GetById(string id)
+        {
+            Event Model;
+            using (ISession session = Helper.OpenSession())
+            {
+                Model = session.Get<Event>(id);
+            }
+            return Model;
+        }
+
+
+        public bool? ToggleStatus(string id, bool status)
+        {
+            Event evnt = GetById(id);
+            if (evnt != null)
+            {
+                bool result = false;
+                if ((bool)evnt.Active != status)
+                {
+                    evnt.Active = status;
+                    this.Update(evnt, "Admin");
+                    result = true;
+                }
+                return result;
+            }
+            return null;
+        }
+
+        public void MarkAsSeen(string id)
+        {
+            Event evnt = GetById(id);
+            if (evnt != null)
+            {
+                evnt.Checked = true;
+                this.Update(evnt, "Admin");
+            }
+        }
+
+        public int Create(Event model)
+        {
+            int EmpNo = 0;
+
+            using (ISession session = Helper.OpenSession())
+            {
+                //Perform transaction
+                using (ITransaction tran = session.BeginTransaction())
+                {
+                    session.Save(model);
+                    tran.Commit();
+                }
+            }
+            return EmpNo;
+        }
+
+        public void Update(Event model, string admin = "NoAdmin")
+        {
+            if (admin == "NoAdmin") model.DateOfCreation = DateTime.Now;
+            model.Checked = true;
+            using (ISession session = Helper.OpenSession())
+            {
+                using (ITransaction tran = session.BeginTransaction())
+                {
+                    session.Update(model);
+                    tran.Commit();
+                }
+            }
+        }
+
+        public void Delete(Event model)
+        {
+            using (ISession session = Helper.OpenSession())
+            {
+                using (ITransaction tran = session.BeginTransaction())
+                {
+                    session.Delete(model);
+                    tran.Commit();
+                }
+            }
+        }
+    }
+}
---
+++ Events.NHibernateDataProvider/DataProviders/NHibernateSubscribersDataProvider.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Events.Business;
+using Events.Business.Models;
+using Events.Business.Interfaces;
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.Transaction;
+
+namespace Events.NHibernateDataProvider.NHibernateCore
+{
+    public class NHibernateSubscribersDataProvider : ISubscribersDataProvider
+    {
+
+        public int GetCount(string EventId)
+        {
+            using (ISession session = Helper.OpenSession())
+            {
+                var criteria = session.CreateCriteria(typeof(Subscribing));
+                criteria.Add(Restrictions.Eq("EventId", EventId));
+                var list = criteria.List<Subscribing>();
+                return list.Count;
+            }
+
+        }
+        public IList<Subscriber> GetAllSubscribers(string EventId)
+        {
+            var result = new List<Subscriber>();
+            var userProvider = new NHibernateUserDataProvider();
+            using (ISession session = Helper.OpenSession())
+            {
+                var criteria = session.CreateCriteria(typeof(Subscribing));
+                criteria.Add(Restrictions.Eq("EventId", EventId));
+                var list = criteria.List<Subscribing>();
+                foreach (var item in list)
+                {
+                    result.Add(AutoMapper.Mapper.Map<Subscriber>(userProvider.GetById(item.UserId)));
+                }
+            }
+            return result;
+        }
+
+       
+        public void SubscribeUser(Subscribing row)
+        {
+            if (IsSubscribed(row)) return;
+            using (ISession session = Helper.OpenSession())
+            {
+                using (ITransaction tran = session.BeginTransaction())
+                {
+                    session.Save(row);
+                    tran.Commit();
+                }
+            }
+        }
+
+        public void UnsubscribeUser(Subscribing row)
+        {
+            if (!IsSubscribed(row)) return;
+            using (ISession session = Helper.OpenSession())
+            {
+                var criteria = session.CreateCriteria<Subscribing>();
+                criteria.Add(Restrictions.Eq("EventId", row.EventId));
+                criteria.Add(Restrictions.Eq("UserId", row.UserId));
+                var rowForDeleting = criteria.List<Subscribing>()[0];
+                using (ITransaction tran = session.BeginTransaction())
+                {
+                    session.Delete(rowForDeleting);
+                    tran.Commit();
+                }
+            }
+        }
+
+        public bool IsSubscribed(Subscribing row)
+        {
+            using (ISession session = Helper.OpenSession())
+            {
+                var criteria = session.CreateCriteria(typeof(Subscribing));
+                criteria.Add(Restrictions.Eq("EventId", row.EventId));
+                criteria.Add(Restrictions.Eq("UserId", row.UserId));
+                var list = criteria.List<Subscribing>();
+                return list != null && list.Count != 0;
+            }
+        }
+    }
+}
