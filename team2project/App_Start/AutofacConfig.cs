67db39a217d28db99dd6cac74dff7f9a5a8aba46
BorysMiller <milbro1995@gmail.com>
2015-06-14 15:28:57 +0300

    configure reminder

--- team2project/App_Start/AutofacConfig.cs
+++ team2project/App_Start/AutofacConfig.cs
@@ -6,6 +6,11 @@ using Events.Business.Classes;
 using Events.Business.Interfaces;
 using Events.NHibernateDataProvider;
 using Events.NHibernateDataProvider.NHibernateCore;
+using Events.NHibernateDataProvider.DataProviders;
+using Quartz.Impl;
+using Events.EmailReminder;
+using Quartz;
+using Quartz.Spi;
 
 namespace team2project
 {
@@ -19,6 +24,7 @@ namespace team2project
             builder.RegisterType<CitiesManager>();
             builder.RegisterType<CommentManager>();
             builder.RegisterType<UserManager>();
+            builder.RegisterType<RemindManager>();
             builder.RegisterType<SubscribersManager>();
             builder.RegisterType<NHibernateSubscribersDataProvider>()
                 .As<ISubscribersDataProvider>();
@@ -35,9 +41,23 @@ namespace team2project
             builder.RegisterType<RuntimeCacheManager>()
                 .As<ICacheManager>();
             builder.RegisterType<NHibernateRoleDataProvider>().AsImplementedInterfaces();
+            builder.RegisterType<NHibernateEmailReminderDataProvider>()
+                .As<IEmailReminderDataProvider>();
+            builder.RegisterType<AutofacJobFactory>().AsImplementedInterfaces();
 
+            builder.Register(x => new StdSchedulerFactory().GetScheduler()).As<IScheduler>();
+            builder.RegisterAssemblyTypes(typeof(EmailRemiderScheduler).Assembly).Where(x => typeof(IJob).IsAssignableFrom(x));
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            IScheduler sched = container.Resolve<IScheduler>();
+            IJobFactory factory = container.Resolve<IJobFactory>();
+            if (sched != null && factory != null)
+            {
+                sched.JobFactory = factory;
+                sched.Start();
+                EmailRemiderScheduler.Start(sched);
+            }
         }
     }
 }
