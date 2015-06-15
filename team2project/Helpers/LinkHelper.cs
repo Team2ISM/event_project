0bef0e77bb53c7aca283c3cc2b4db29ea0b7781a
BilyachenkoOY <iviviv1901@gmail.com>
2015-06-16 00:36:38 +0300

    buttons

---
+++ team2project/Helpers/LinkHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web.Mvc.Html;
+
+namespace team2project.Helpers
+{
+    public static class LinkHelper
+    {
+        public static HtmlString CreateLink(this HtmlHelper html, string innerHtml, string routeName, object routeValues, object htmlAttributes)
+        {
+            var a = html.RouteLink(innerHtml, routeName, routeValues, htmlAttributes);
+            return MvcHtmlString.Create(HttpUtility.HtmlDecode(a.ToHtmlString()));            
+        }
+    }
+}
