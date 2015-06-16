e51dde15a6df7b4cb2c28f8212d184603abb5504
AlexSolari @ PC <gearbots@yandex.com>
2015-06-16 22:12:12 +0300

    Reworked dropdown

--- team2project/Scripts/listScript.js
+++ team2project/Scripts/listScript.js
@@ -1,156 +1,14 @@
-﻿(function ($) {
-    $.widget("custom.combobox", {
-        _create: function () {
-            this.wrapper = $("<span>")
-              .addClass("custom-combobox")
-              .insertAfter(this.element);
-
-            this.element.hide();
-            this._createAutocomplete();
-            this._createShowAllButton();
-        },
-
-        _createAutocomplete: function () {
-            var selected = this.element.children(":selected"),
-              value = selected.val() ? selected.text() : "";
-
-            this.input = $("<input>")
-              .appendTo(this.wrapper)
-              .val(value)
-              .attr("title", "")
-              .addClass("custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left")
-              .autocomplete({
-                  delay: 0,
-                  minLength: 0,
-                  source: $.proxy(this, "_source")
-              })
-              .tooltip({
-                  tooltipClass: "ui-state-highlight"
-              });
-
-            this._on(this.input, {
-                autocompleteselect: function (event, ui) {
-                    ui.item.option.selected = true;
-                    this._trigger("select", event, {
-                        item: ui.item.option
-                    });
-
-                    var url = location.protocol + '//' + location.host + '/', path = location.pathname.split('/');
-                    url += path[1];
-
-                    if (period && period !== '-1') {
-                        url += '/' + period;
-                    }
-
-                    if (ui.item.value && ui.item.option.value !== '-1' && ui.item.value !== "Все") {
-                        url += '/' + ui.item.option.value;
-                    }
-                    document.location.href = url;
-                    setTimeout(function () {
-                        document.location.href = url;
-                    }, 1000);
-                },
-
-                autocompletechange: "_removeIfInvalid"
-            });
-        },
-
-        _createShowAllButton: function () {
-            var input = this.input,
-              wasOpen = false;
-
-            $("<a>")
-              .attr("tabIndex", -1)
-              .attr("title", "Показать все города")
-              .tooltip()
-              .appendTo(this.wrapper)
-              .button({
-                  icons: {
-                      primary: "ui-icon-triangle-1-s"
-                  },
-                  text: false
-              })
-              .removeClass("ui-corner-all")
-              .addClass("custom-combobox-toggle ui-corner-right")
-              .mousedown(function () {
-                  wasOpen = input.autocomplete("widget").is(":visible");
-              })
-              .click(function () {
-                  input.focus();
-
-                  // Close if already visible
-                  if (wasOpen) {
-                      return;
-                  }
-
-                  // Pass empty string as value to search for, displaying all results
-                  input.autocomplete("search", "");
-              });
-        },
-
-        _source: function (request, response) {
-            var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
-            response(this.element.children("option").map(function () {
-                var text = $(this).text();
-                if (this.value && (!request.term || matcher.test(text)))
-                    return {
-                        label: text,
-                        value: text,
-                        option: this
-                    };
-            }));
-        },
-
-        _removeIfInvalid: function (event, ui) {
-
-            // Selected an item, nothing to do
-            if (ui.item) {
-                return;
-            }
-
-            // Search for a match (case-insensitive)
-            var value = this.input.val(),
-              valueLowerCase = value.toLowerCase(),
-              valid = false;
-            this.element.children("option").each(function () {
-                if ($(this).text().toLowerCase() === valueLowerCase) {
-                    this.selected = valid = true;
-                    return false;
-                }
-            });
-
-            // Found a match, nothing to do
-            if (valid) {
-                return;
-            }
-
-            // Remove invalid value
-            this.input
-              .val("")
-              .attr("title", value + " didn't match any item")
-              .tooltip("open");
-            this.element.val("");
-            this._delay(function () {
-                this.input.tooltip("close").attr("title", "");
-            }, 2500);
-            this.input.autocomplete("instance").term = "";
-        },
-
-        _destroy: function () {
-            this.wrapper.remove();
-            this.element.show();
-        }
-    });
-})(jQuery);
-
-var combobox = $("#combobox");
-var period, city;
+﻿var period, city;
 $(function () {
     var path = decodeURI(location.pathname).split('/');
 
+    var select = $("#City");
+
+    select.prepend('<option value="-1">Все</option>');
+
     period = path[2];
     if (path[3]) {
-        var arr = combobox.children();
+        var arr = select.children();
         if (cityValue) {
             arr.each(function (i, val) {
                 if ($(val).val() === cityValue) { val.setAttribute('selected', 'selected'); city = val; return false }
@@ -158,19 +16,23 @@ $(function () {
         }
     }
 
-    combobox.html('<option value="-1">Все</option>' + combobox.html());
-    combobox.combobox();
-    $("#toggle").click(function () {
-        combobox.toggle();
+    select.on("change", function () {
+        var url = location.protocol + '//' + location.host + '/', path = location.pathname.split('/');
+        url += path[1];
+
+        if (period && period !== '-1') {
+            url += '/' + period;
+        }
+
+        if ($("#City option:selected" ).text() !== "Все") {
+            url += '/' + $(this).val();
+        }
+        document.location.href = url;
+        setTimeout(function () {
+            document.location.href = url;
+        }, 1000);
     });
 
-    /*$(".zoomEnabled").zoom({
-        duration: 150,
-        magnify:0.3
-    });*/
-});
-
-$(function () {
     $('.date-filters a').each(function () {
         var location = window.location.href;
         var link = this.href;
@@ -179,8 +41,7 @@ $(function () {
         }
     });
 
-    $(".filter-link").each(function() {
-        $(this).attr("href",$(this).attr("href") + "/" + cityValue);
+    $(".filter-link").each(function () {
+        $(this).attr("href", $(this).attr("href") + "/" + cityValue);
     });
-
 });
