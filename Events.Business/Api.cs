929ad96a34812d1d78da86e387aec746e3781b09
Vlad Stepchin <vladmasterman@gmail.com>
2015-06-17 17:02:17 +0300

    adding to previous commit(2)

--- Events.Business/Api.cs
+++ Events.Business/Api.cs
@@ -14,13 +14,20 @@ namespace Events.Business
         {
             get { return DependencyResolver.Current.GetService<CommentManager>(); }
         }
+
         public static RemindManager RemindManager
         {
             get { return DependencyResolver.Current.GetService<RemindManager>(); }
         }
+
         public static CitiesManager CitiesManager 
         {
             get { return DependencyResolver.Current.GetService<CitiesManager>(); }
+        }
+
+        public static UserManager UserManager 
+        {
+            get { return DependencyResolver.Current.GetService<UserManager>(); }
         }
     }
 }
